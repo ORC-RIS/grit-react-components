92b0d6626a32e6d227deed27915a7e7011736185
mark-durbin-ucf <Mark.Durbin@ucf.edu>
2021-09-02 13:06:03 -0400

    Added react-boostrap to project. Modified MainNavigation component.

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "MainNavigation", {
+  enumerable: true,
+  get: function get() {
+    return _MainNavigation.default;
+  }
+});
+
+var _MainNavigation = _interopRequireDefault(require("./MainNavigation/MainNavigation"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
--- src/components/MainNavigation/MainNavigation.js
+++ src/components/MainNavigation/MainNavigation.js
@@ -1,11 +1,71 @@
-import React from 'react'
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
-const MainNavigation = () => {
+const MainNavigation = ({ config = {} }) => {
     return (
-        <div>
-            MainNavigation
+        <div className="MainNavigation">
+            <div className="main-navigation-wrapper">
+                <Navbar
+                    collapseOnSelect
+                    className={`main-navigation ${
+                        config.overlapContent ? "overlap-content" : ""
+                    }`}
+                >
+                    <Container>
+                        <NavbarBrand as={config.brandAs} content={config.brandContent} />
+                        {config.links && <NavbarToggle />}
+                        {config.links && (
+                            <Navbar.Collapse id="navbarScroll">
+                                <MainNav links={config.links} />
+                            </Navbar.Collapse>
+                        )}
+                    </Container>
+                </Navbar>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default MainNavigation
+const MainNav = ({ links = [] }) => {
+    return (
+        <Nav className="main-nav" as="ul" activeKey="">
+            {links &&
+                links.map((link, idx) => {
+                    return (
+                        <Nav.Item as="li" key={idx}>
+                            <Nav.Link
+                                as={link.as}
+                                activeClassName=""
+                                className={
+                                    link.className ? link.className : false
+                                }
+                                to={link.href}
+                                target={link.target}
+                            >
+                                {link.text}
+                            </Nav.Link>
+                        </Nav.Item>
+                    );
+                })}
+        </Nav>
+    );
+};
+
+const NavbarBrand = ({ as, content }) => {
+    return (
+        <Navbar.Brand as={as} to="/">
+            {content}
+        </Navbar.Brand>
+    );
+};
+
+const NavbarToggle = () => {
+    return (
+        <Navbar.Toggle>
+            <span className="navbar-toggler-text">Navigation</span>
+            <span className="navbar-toggler-icon ml-2"></span>
+        </Navbar.Toggle>
+    );
+};
+
+export default MainNavigation;
