21224df92ad76c833d8d915774dc0e62f80c141c
mark-durbin-ucf <Mark.Durbin@ucf.edu>
2021-09-16 13:59:39 -0400

    Added missing story for AppHeader component.

---
+++ src/lib/components/application/AppHeader/AppHeader.stories.js
@@ -0,0 +1,43 @@
+import React from "react";
+import AppHeader from "./AppHeader";
+import "./../../../../scss/style.scss";
+
+export default {
+    title: "Application/AppHeader",
+    component: AppHeader,
+};
+
+const Template = (args) => <AppHeader {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+    config: {
+        appNavigation: {
+            brandContent: "My GRIT App",
+            links: [
+                {
+                    text: "Page Title",
+                    href: "#",
+                },
+                {
+                    text: "About",
+                    href: "#",
+                },
+                {
+                    text: <>Products &amp; Services</>,
+                    href: "#",
+                },
+                {
+                    text: "Application-specific Page",
+                    href: "#",
+                },
+                {
+                    text: "Contact Us",
+                    className: "bg-primary text-secondary text-shadow-none",
+                    href: "#",
+                },
+            ],
+        },
+    },
+};
