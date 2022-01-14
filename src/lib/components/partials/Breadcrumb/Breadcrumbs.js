6fe23f65a027f29b27c196e82a815637acea2c63
mark-durbin-ucf <mark.durbin@ucf.edu>
2022-01-14 13:31:52 -0500

    Corrected to attribute.

--- src/lib/components/partials/Breadcrumb/Breadcrumbs.js
+++ src/lib/components/partials/Breadcrumb/Breadcrumbs.js
@@ -59,7 +59,7 @@ const CustomBreadcrumb = ({ config }) => {
                     return (
                         <Breadcrumb.Item
                             href={link.href ? link.href : false}
-                            to={link.href ? link.href : false}
+                            to={link.to ? link.to : false}
                             linkAs={config.linksAs}
                             linkProps={link}
                             key={idx}
